ec7535defa9be4cb61958a305d1d6eb38f46d145
Cleiton Farias <cleiton.araujo.farias@gmail.com>
2022-04-17 19:38:50 -0300

    feat: added remote=authentication test

--- src/a.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('aa', () => {
-  test('aa', () => {
-    expect(true).toBe(true)
-  })
-})
---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,16 @@
+class RemoteAuthentication {
+  constructor (private readonly url: string) {}
+
+  async auth (): Promise<void> {
+    return Promise.resolve()
+  }
+}
+
+describe('RemoteAuthentication',  () => {
+  test('Should call HttpClient with correct URL', async () => {
+    const url = 'any_url'
+    const sut = new RemoteAuthentication(url)
+    await sut.auth()
+    expect(httpClient.url).toBe(url)
+  })
+})
