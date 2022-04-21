35602b7f27cbc2bc9bca932560e1e6900f44fcc6
cleitonfarias93 <cleiton.araujo.farias@gmail.com>
2022-04-21 16:37:07 -0300

    test: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,3 +1,4 @@
+import { HttpPostParams } from '@/data/protocols/http'
 import { AxiosHttpClient } from './axios-http-client'
 import axios from 'axios'
 import faker from '@faker-js/faker'
@@ -9,12 +10,17 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement({ key: faker.random.alphaNumeric(10) })
+})
+
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and verb', async () => {
-    const url = faker.internet.url()
+  test('Should call axios with correct values', async () => {
+    const postRequest = mockPostRequest()
 
     const sut = makeSut()
-    await sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+    await sut.post(postRequest)
+    expect(mockedAxios.post).toHaveBeenCalledWith(postRequest.url, postRequest.body)
   })
 })
